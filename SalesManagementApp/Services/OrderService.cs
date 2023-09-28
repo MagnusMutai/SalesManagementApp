0aa0af33b8651e1697d219ae523eec11e79e620b
Gilly <mutaigilly02@gmail.com>
2023-09-28 12:19:26 +0300

    Continuosly

--- SalesManagementApp/Services/OrderService.cs
+++ SalesManagementApp/Services/OrderService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using SalesManagementApp.Data;
+﻿using SalesManagementApp.Data;
 using SalesManagementApp.Entities;
 using SalesManagementApp.Models;
 using SalesManagementApp.Services.Contracts;
@@ -35,8 +34,6 @@ namespace SalesManagementApp.Services
                 var orderItemsToAdd = ReturnOrderItemsWithOrderId(orderId, orderModel.OrderItems);
                 this.salesManagementDbContext.AddRange(orderItemsToAdd);
                 await this.salesManagementDbContext.SaveChangesAsync();
-
-                await UpdateSalesOrderReportsTable(orderId, order);
             }
             catch (Exception)
             {
@@ -55,45 +52,5 @@ namespace SalesManagementApp.Services
                         ProductId = oi.ProductId
                     }).ToList();
         }
-        private async Task UpdateSalesOrderReportsTable(int orderId, Order order )
-        {
-            try
-            {
-                List<SalesOrderReport> srItems = await (from oi in this.salesManagementDbContext.OrderItems
-                                                        where oi.OrderId == orderId
-                                                        select new SalesOrderReport
-                                                        {
-                                                            OrderId = orderId,
-                                                            OrderDateTime = order.OrderDateTime,
-                                                            OrderPrice = order.Price,
-                                                            OrderQty = order.Qty,
-                                                            OrderItemId = oi.Id,
-                                                            OrderItemPrice = oi.Price,
-                                                            OrderItemQty = oi.Qty,
-                                                            EmployeeId = order.EmployeeId,
-                                                            EmployeeFirstName = salesManagementDbContext.Employees.FirstOrDefault(e => e.Id == order.EmployeeId).FirstName,
-                                                            EmployeeLastName = salesManagementDbContext.Employees.FirstOrDefault(e => e.Id == order.EmployeeId).LastName,
-                                                            ProductId = oi.ProductId,
-                                                            ProductName = salesManagementDbContext.Products.FirstOrDefault(p => p.Id == oi.ProductId).Name,
-                                                            ProductCategoryId = salesManagementDbContext.Products.FirstOrDefault(p => p.Id == oi.ProductId).CategoryId,
-                                                            ProductCategoryName = salesManagementDbContext.ProductCategories.FirstOrDefault(c => c.Id == salesManagementDbContext.Products.FirstOrDefault(p => p.Id == oi.ProductId).CategoryId).Name,
-                                                            ClientId = order.ClientId,
-                                                            ClientFirstName = salesManagementDbContext.Clients.FirstOrDefault(c => c.Id == order.ClientId).FirstName,
-                                                            ClientLastName = salesManagementDbContext.Clients.FirstOrDefault(c => c.Id == order.ClientId).LastName,
-                                                            RetailOutletId = salesManagementDbContext.Clients.FirstOrDefault(c => c.Id == order.ClientId).RetailOutletId,
-                                                            RetailOutletLocation = salesManagementDbContext.RetailOutlets.FirstOrDefault(r => r.Id == salesManagementDbContext.Clients.FirstOrDefault(c => c.Id == order.ClientId).RetailOutletId).Location
-                                                        }).ToListAsync();
-                this.salesManagementDbContext.AddRange(srItems);
-                await this.salesManagementDbContext.SaveChangesAsync();
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-        }
-
     }
-
 }
-
